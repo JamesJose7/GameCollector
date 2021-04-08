4282b3f6158a003143d596b5cc2be4db0b52d82b
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2021-04-08 13:00:23 -0500

    fixed untracked files

--- app/src/main/java/com/jeeps/gamecollector/services/igdb/IgdbApiClient.java
+++ app/src/main/java/com/jeeps/gamecollector/services/igdb/IgdbApiClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class IgdbApiClient {
     private static final String BASE_URL = "https://api-v3.igdb.com/";
-    private static final String API_KEY = "YOUR_API_KEY_HERE";
+    private static final String API_KEY = "6c2e3bc7e808d77d2d0023f5ea715fff";
 
     private static Retrofit.Builder builder =
             new Retrofit.Builder()
