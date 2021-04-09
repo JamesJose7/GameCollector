fdd86f8622d48c88da5533a704270428c4ebbc66
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2021-04-09 15:09:28 -0500

    Implemented backend function to toggle game completion

--- app/src/main/java/com/jeeps/gamecollector/GameDetailsActivity.java
+++ app/src/main/java/com/jeeps/gamecollector/GameDetailsActivity.java
@@ -2,16 +2,20 @@ package com.jeeps.gamecollector;
 
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.transition.Explode;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,14 +23,25 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.palette.graphics.Palette;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.jeeps.gamecollector.model.CurrentUser;
 import com.jeeps.gamecollector.model.Game;
+import com.jeeps.gamecollector.model.ToggleCompletionResponse;
+import com.jeeps.gamecollector.services.api.ApiClient;
+import com.jeeps.gamecollector.services.api.GameService;
+import com.jeeps.gamecollector.utils.UserUtils;
 import com.squareup.picasso.Picasso;
+import com.varunest.sparkbutton.SparkButton;
+import com.varunest.sparkbutton.SparkEventListener;
 
 import java.io.IOException;
 import java.net.URL;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static com.jeeps.gamecollector.PlatformLibraryActivity.CURRENT_PLATFORM;
 import static com.jeeps.gamecollector.PlatformLibraryActivity.CURRENT_PLATFORM_NAME;
@@ -36,6 +51,8 @@ import static com.jeeps.gamecollector.PlatformLibraryActivity.SELECTED_GAME;
 import static com.jeeps.gamecollector.PlatformLibraryActivity.SELECTED_GAME_POSITION;
 
 public class GameDetailsActivity extends AppCompatActivity {
+
+    private static final String TAG = GameDetailsActivity.class.getSimpleName();
 
     @BindView(R.id.game_cover)
     ImageView gameCoverView;
@@ -47,12 +64,15 @@ public class GameDetailsActivity extends AppCompatActivity {
     TextView gamePlatformText;
     @BindView(R.id.fab)
     FloatingActionButton fabButton;
+    @BindView(R.id.complete_switch)
+    SparkButton completeSwitch;
 
+    private Toolbar toolbar;
     private String platformId;
     private String platformName;
     private Game selectedGame;
     private int selectedGamePosition;
-    private Toolbar toolbar;
+    private CurrentUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +86,11 @@ public class GameDetailsActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ButterKnife.bind(this);
+
+        // Get local data
+        SharedPreferences sharedPreferences = getSharedPreferences(
+                getString(R.string.shared_preferences_global), Context.MODE_PRIVATE);
+        currentUser = UserUtils.getCurrentUser(this, sharedPreferences);
 
         //Get intent contents
         Intent intent = getIntent();
@@ -102,9 +127,55 @@ public class GameDetailsActivity extends AppCompatActivity {
             gamePublisherText.setText(selectedGame.getPublisher());
         gamePlatformText.setText(selectedGame.getPlatform());
 
+        // Game completion button
+        completeSwitch.setChecked(selectedGame.getTimesCompleted() > 0);
+        setupCompleteSwitch();
+
         fabButton.setOnClickListener(view -> editGame());
 
         getCoverColors();
+    }
+
+    private void setupCompleteSwitch() {
+        completeSwitch.setEventListener(new SparkEventListener() {
+            @Override
+            public void onEvent(ImageView button, boolean buttonState) {
+                updateGameCompletion(buttonState);
+            }
+
+            @Override
+            public void onEventAnimationEnd(ImageView button, boolean buttonState) { }
+
+            @Override
+            public void onEventAnimationStart(ImageView button, boolean buttonState) {}
+        });
+    }
+
+    private void updateGameCompletion(boolean completed) {
+        GameService gameService = ApiClient.createService(GameService.class);
+        Call<ToggleCompletionResponse> toggleGameCompletion = gameService.toggleGameCompletion("Bearer " + currentUser.getToken(),
+                selectedGame.getId());
+        toggleGameCompletion.enqueue(new Callback<ToggleCompletionResponse>() {
+            @Override
+            public void onResponse(Call<ToggleCompletionResponse> call, Response<ToggleCompletionResponse> response) {
+                if (response.isSuccessful()) {
+                    ToggleCompletionResponse completionResponse = response.body();
+                    String message = completionResponse.isCompleted() ?
+                            "Marked as complete" :
+                            "Marked as incomplete";
+                    Snackbar.make(fabButton, message, Snackbar.LENGTH_SHORT).show();
+                } else {
+                    Log.e(TAG, "Toggle game completion request failed");
+                    Toast.makeText(GameDetailsActivity.this, "There was an error when updating the game, please try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ToggleCompletionResponse> call, Throwable t) {
+                Log.e(TAG, "Toggle game completion request failed");
+                Toast.makeText(GameDetailsActivity.this, "There was an error when updating the game, please try again", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void getCoverColors() {
@@ -125,7 +196,6 @@ public class GameDetailsActivity extends AppCompatActivity {
 
     private void animateStatusBarColor(int colorTo) {
         int colorFrom = getResources().getColor(R.color.colorPrimary);
-//        int colorTo = Color.parseColor("#00ff00");
         ObjectAnimator colorAnimation = ObjectAnimator.ofObject(toolbar, "backgroundColor",
                 new ArgbEvaluator(), colorFrom, colorTo);
         colorAnimation.setDuration(300);
