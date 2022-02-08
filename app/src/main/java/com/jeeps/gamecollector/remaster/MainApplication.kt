a67e49400549e9762334e69a9dc5cd1d920ecbed
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2022-02-08 15:48:52 -0500

    Add hilt dependency injection

---
+++ app/src/main/java/com/jeeps/gamecollector/remaster/MainApplication.kt
@@ -0,0 +1,10 @@
+package com.jeeps.gamecollector.remaster
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application() {
+
+
+}
