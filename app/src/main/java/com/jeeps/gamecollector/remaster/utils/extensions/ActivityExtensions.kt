ad76d723a7adc4b972336f2c5840dacc062468e8
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2022-02-11 19:05:04 -0500

    Add snack bar and toast extensions on activity

--- app/src/main/java/com/jeeps/gamecollector/remaster/utils/extensions/ActivityExtensions.kt
+++ app/src/main/java/com/jeeps/gamecollector/remaster/utils/extensions/ActivityExtensions.kt
@@ -1,11 +1,30 @@
 package com.jeeps.gamecollector.remaster.utils.extensions
 
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
+import com.google.android.material.snackbar.Snackbar
 
 inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
     crossinline bindingInflater: (LayoutInflater) -> T) =
     lazy(LazyThreadSafetyMode.NONE) {
         bindingInflater.invoke(layoutInflater)
     }
+
+
+fun AppCompatActivity.showSnackBar(
+    rootView: View,
+    message: String,
+    length: Int = Snackbar.LENGTH_SHORT
+) {
+    Snackbar.make(rootView, message, length).show()
+}
+
+fun AppCompatActivity.showToast(
+    message: String,
+    length: Int = Toast.LENGTH_SHORT
+) {
+    Toast.makeText(this, message, length).show()
+}
