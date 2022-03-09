252514577887fd8d84018b45aa37144e4e6c18ee
José Eduardo Eguiguren <pepe.96.jose@gmail.com>
2022-03-09 13:31:41 -0500

    Add string similarity extension

--- app/src/main/java/com/jeeps/gamecollector/remaster/utils/extensions/StringExtensions.kt
+++ app/src/main/java/com/jeeps/gamecollector/remaster/utils/extensions/StringExtensions.kt
@@ -3,3 +3,40 @@ package com.jeeps.gamecollector.remaster.utils.extensions
 fun String.bearer(): String {
     return "Bearer $this"
 }
+
+fun String.similarity(target: String): Double {
+    var longer: String = this
+    var shorter: String? = target
+    if (this.length < target.length) { // longer should always have greater length
+        longer = target
+        shorter = this
+    }
+    val longerLength = longer.length
+    return if (longerLength == 0) {
+        1.0 /* both strings are zero length */
+    } else (longerLength - editDistance(longer, shorter)) / longerLength.toDouble()
+}
+
+private fun editDistance(s1: String, s2: String?): Int {
+    val string1 = s1.lowercase()
+    val string2 = s2?.lowercase() ?: ""
+    val costs = IntArray(string2.length + 1)
+    for (i in 0..string1.length) {
+        var lastValue = i
+        for (j in 0..string2.length) {
+            if (i == 0) costs[j] = j else {
+                if (j > 0) {
+                    var newValue = costs[j - 1]
+                    if (string1[i - 1] != string2[j - 1]) newValue = Math.min(
+                        Math.min(newValue, lastValue),
+                        costs[j]
+                    ) + 1
+                    costs[j - 1] = lastValue
+                    lastValue = newValue
+                }
+            }
+        }
+        if (i > 0) costs[string2.length] = lastValue
+    }
+    return costs[string2.length]
+}
