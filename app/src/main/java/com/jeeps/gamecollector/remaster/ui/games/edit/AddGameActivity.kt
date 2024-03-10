e58d6e0cbc9fbf047af2ecf81efcd99a31c4716d
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2024-03-10 01:38:22 -0800

    Create compose screen for adding/editing games

--- app/src/main/java/com/jeeps/gamecollector/remaster/ui/games/edit/AddGameActivity.kt
+++ app/src/main/java/com/jeeps/gamecollector/remaster/ui/games/edit/AddGameActivity.kt
@@ -1,16 +1,61 @@
 package com.jeeps.gamecollector.remaster.ui.games.edit
 
 import android.content.Intent
-import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.compose.AppTheme
 import com.jeeps.gamecollector.R
 import com.jeeps.gamecollector.databinding.ActivityAddGameBinding
 import com.jeeps.gamecollector.databinding.ContentAddGameBinding
@@ -27,6 +72,8 @@ import com.jeeps.gamecollector.remaster.utils.extensions.viewBinding
 import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.math.roundToInt
+import android.graphics.Color as GraphicsColor
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
@@ -89,14 +136,14 @@ class AddGameActivity : BaseActivity() {
         viewModel.coverDeleted = false
         viewModel.setGameImageUri(uri)
         Picasso.get().load(uri).into(content.gameCover)
-        content.gameCover.setBackgroundColor(Color.parseColor("#99cccccc"))
+        content.gameCover.setBackgroundColor(GraphicsColor.parseColor("#99cccccc"))
     }
 
     private fun removeImageCover() {
         viewModel.coverDeleted = true
         viewModel.setGameImageUri(null)
         content.gameCover.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_add_image))
-        content.gameCover.setBackgroundColor(Color.parseColor("#cccccc"))
+        content.gameCover.setBackgroundColor(GraphicsColor.parseColor("#cccccc"))
     }
 
     private fun checkIfGameIsBeingEdited() {
@@ -114,11 +161,11 @@ class AddGameActivity : BaseActivity() {
             // Images
             if (game.imageUri.isNotEmpty()) {
                 Picasso.get().load(game.imageUri).into(content.gameCover)
-                content.gameCover.setBackgroundColor(Color.parseColor("#99cccccc"))
+                content.gameCover.setBackgroundColor(GraphicsColor.parseColor("#99cccccc"))
             } else {
                 viewModel.coverDeleted = true
                 Picasso.get().load(R.drawable.edit_picture).into(content.gameCover)
-                content.gameCover.setBackgroundColor(Color.parseColor("#cccccc"))
+                content.gameCover.setBackgroundColor(GraphicsColor.parseColor("#cccccc"))
             }
             // Names
             content.gameNameEdit.setText(game.name)
@@ -224,5 +271,186 @@ class AddGameActivity : BaseActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun AddGameScreen(
+    modifier: Modifier = Modifier,
+    name: String,
+    shortName: String,
+    platform: String,
+    publisher: String,
+    isPhysical: Boolean,
+    timesCompleted: Int,
+    coverImageUri: Uri? = null,
+    onNameChange: (String) -> Unit = { },
+    onShortNameChange: (String) -> Unit = { },
+    onPlatformChange: (String) -> Unit = { },
+    onPublisherChange: (String) -> Unit = { },
+    onIsPhysicalChange: (Boolean) -> Unit = { },
+    onTimesCompletedChange: (Int) -> Unit = { },
+    onCoverImageChange: (Uri?) -> Unit = { },
+) {
+    var selectedCover by rememberSaveable { mutableStateOf(coverImageUri) }
+    val pickMedia = rememberLauncherForActivityResult(contract = PickVisualMedia()) { uri ->
+        selectedCover = uri
+        onCoverImageChange(uri)
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Box {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(selectedCover)
+                    .placeholder(R.drawable.ic_add_image)
+                    .fallback(R.drawable.ic_add_image)
+                    .build(),
+                contentScale = ContentScale.Fit,
+                contentDescription = "Game cover",
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+                    .background(color = Color(0xFFCCCCCC))
+                    .clickable {
+                        pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
+                    }
+            )
+            IconButton(
+                onClick = {
+                    selectedCover = null
+                    onCoverImageChange(null)
+                },
+                modifier = Modifier.align(Alignment.TopEnd)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.size(20.dp),
+                )
+            }
+        }
+        Text(
+            text = stringResource(id = R.string.cover_disclaimer),
+            color = colorResource(id = R.color.textSecondaryColor),
+            modifier = Modifier.padding(horizontal = 10.dp)
+        )
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier.padding(all = 10.dp)
+        ) {
+            OutlinedTextField(
+                value = name,
+                onValueChange = onNameChange,
+                label = { Text(text = "Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = shortName,
+                onValueChange = onShortNameChange,
+                label = { Text(text = "Short Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                OutlinedTextField(
+                    value = platform,
+                    onValueChange = onPlatformChange,
+                    label = { Text(text = "Platform") },
+                    modifier = Modifier.weight(1f)
+                )
+                Spacer(modifier = Modifier.size(10.dp))
+                OutlinedTextField(
+                    value = publisher,
+                    onValueChange = onPublisherChange,
+                    label = { Text(text = "Publisher") },
+                    modifier = Modifier.weight(1f)
+                )
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
+            ) {
+                Text(text = "Digital", modifier = Modifier.clickable { onIsPhysicalChange(false) })
+                Switch(
+                    checked = isPhysical,
+                    onCheckedChange = onIsPhysicalChange,
+                    thumbContent = if (isPhysical) {
+                        {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_physical),
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.primary,
+                                modifier = Modifier.size(SwitchDefaults.IconSize),
+                            )
+                        }
+                    } else {
+                        {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_digital),
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.primary,
+                                modifier = Modifier.size(SwitchDefaults.IconSize),
+                            )
+                        }
+                    },
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = MaterialTheme.colorScheme.surface,
+                        checkedTrackColor = MaterialTheme.colorScheme.primary,
+                        checkedBorderColor = MaterialTheme.colorScheme.primary,
+                        uncheckedThumbColor = MaterialTheme.colorScheme.surface,
+                        uncheckedTrackColor = MaterialTheme.colorScheme.secondary,
+                        uncheckedBorderColor = MaterialTheme.colorScheme.secondary,
+                    ),
+                    modifier = Modifier.padding(horizontal = 20.dp)
+                )
+                Text(text = "Physical", modifier = Modifier.clickable { onIsPhysicalChange(true) })
+            }
+
+            Column {
+                Slider(
+                    value = timesCompleted.toFloat(),
+                    onValueChange = { onTimesCompletedChange(it.roundToInt()) },
+                    colors = SliderDefaults.colors(
+                        thumbColor = MaterialTheme.colorScheme.secondary,
+                        activeTrackColor = MaterialTheme.colorScheme.secondary,
+                        inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+                    ),
+                    steps = 5,
+                    valueRange = 0f..5f
+                )
+                Text(text = "Times completed: $timesCompleted")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AddGameScreenPreview() {
+    var isPhysical by remember { mutableStateOf(true) }
+    var timesCompleted by remember { mutableIntStateOf(0) }
+    AppTheme {
+        AddGameScreen(
+            name = "",
+            shortName = "",
+            platform = "",
+            publisher = "",
+            isPhysical = isPhysical,
+            timesCompleted = timesCompleted,
+            onIsPhysicalChange = { isPhysical = it },
+            onTimesCompletedChange = { timesCompleted = it}
+        )
     }
 }
