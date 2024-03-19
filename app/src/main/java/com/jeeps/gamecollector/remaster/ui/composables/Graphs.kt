01711e46dae9d44612ed07cdc43d989b00d16138
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2024-03-18 22:53:45 -0700

    Add initial stats compose card

---
+++ app/src/main/java/com/jeeps/gamecollector/remaster/ui/composables/Graphs.kt
@@ -0,0 +1,91 @@
+package com.jeeps.gamecollector.remaster.ui.composables
+
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.compose.AppTheme
+
+@Composable
+fun CircularGraph(
+    modifier: Modifier = Modifier,
+    percentage: Float,
+    strokeWidth: Dp = 12.dp,
+    strokeColor: Color = MaterialTheme.colorScheme.tertiary,
+    size: Dp = 120.dp,
+    animationSpec: AnimationSpec<Float> = tween(
+        durationMillis = 600,
+        easing = FastOutSlowInEasing
+    )
+) {
+    val barProgress: Float by animateFloatAsState(
+        targetValue = percentage,
+        label = "Bar animation",
+        animationSpec = animationSpec
+    )
+
+    Box(
+        modifier = modifier
+    ) {
+        CircularProgressIndicator(
+            progress = { barProgress },
+            strokeCap = StrokeCap.Round,
+            strokeWidth = strokeWidth,
+            color = strokeColor,
+            modifier = Modifier.size(size)
+        )
+        Text(
+            text = "100%",
+            fontSize = 30.sp,
+            color = MaterialTheme.colorScheme.onTertiaryContainer,
+            modifier = Modifier
+                .align(Alignment.Center)
+        )
+        Text(
+            text = "Completed",
+            fontSize = 10.sp,
+            color = MaterialTheme.colorScheme.onTertiaryContainer,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(top = 40.dp)
+        )
+
+    }
+
+}
+
+@Preview
+@Composable
+private fun CircularGraphPreview() {
+    AppTheme {
+        Surface {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                CircularGraph(percentage = 0.1f)
+                CircularGraph(percentage = 0.5f)
+                CircularGraph(percentage = 0.8f)
+                CircularGraph(percentage = 1f)
+            }
+        }
+    }
+}
--- app/src/main/java/com/jeeps/gamecollector/remaster/ui/userStats/UserStatsActivity.kt
+++ app/src/main/java/com/jeeps/gamecollector/remaster/ui/userStats/UserStatsActivity.kt
@@ -3,15 +3,40 @@ package com.jeeps.gamecollector.remaster.ui.userStats
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.compose.AppTheme
 import com.google.android.material.snackbar.Snackbar
 import com.jeeps.gamecollector.R
-import com.jeeps.gamecollector.remaster.ui.adapters.PlatformStatsAdapter
 import com.jeeps.gamecollector.databinding.ActivityStatsBinding
 import com.jeeps.gamecollector.databinding.ContentStatsBinding
 import com.jeeps.gamecollector.remaster.data.model.data.platforms.PlatformStats
 import com.jeeps.gamecollector.remaster.data.model.data.user.UserStats
+import com.jeeps.gamecollector.remaster.ui.adapters.PlatformStatsAdapter
 import com.jeeps.gamecollector.remaster.ui.base.BaseActivity
+import com.jeeps.gamecollector.remaster.ui.composables.CircularGraph
 import com.jeeps.gamecollector.remaster.utils.extensions.completionPercentage
 import com.jeeps.gamecollector.remaster.utils.extensions.totalGames
 import com.jeeps.gamecollector.remaster.utils.extensions.value
@@ -106,4 +131,132 @@ class UserStatsActivity : BaseActivity() {
         content.platformStatsRecyclerview.adapter = platformsStatsAdapter
     }
 
+}
+
+@Composable
+fun UserStatsScreen(
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier
+            .background(Color.White)
+            .padding(all = 10.dp)
+            .fillMaxSize()
+    ) {
+        StatCard {
+            Text(
+                text = "Overall",
+                color = MaterialTheme.colorScheme.onSurface,
+                style = MaterialTheme.typography.headlineLarge,
+            )
+            CircularGraph(
+                percentage = 1f,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                StatChip(
+                    value = "100",
+                    label = "Total",
+                    modifier = Modifier
+                        .weight(50f)
+                )
+                StatChip(
+                    value = "33",
+                    label = "Completed",
+                    modifier = Modifier
+                        .weight(50f)
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                StatChip(
+                    value = "55",
+                    label = "Physical",
+                    modifier = Modifier
+                        .weight(50f)
+                )
+                StatChip(
+                    value = "45",
+                    label = "Digital",
+                    modifier = Modifier
+                        .weight(50f)
+                )
+            }
+            StatChip(
+                value = "The legend of Zelda",
+                label = "Last Game Completed",
+                valueFontSize = 20.sp
+            )
+
+        }
+    }
+}
+
+@Composable
+fun StatChip(
+    modifier: Modifier = Modifier,
+    value: String,
+    label: String,
+    valueFontSize: TextUnit = 30.sp
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colorScheme.primaryContainer,
+                shape = RoundedCornerShape(percent = 50)
+            )
+    ) {
+        Text(
+            text = value,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            fontSize = valueFontSize,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 8.dp, start = 18.dp, end = 18.dp)
+        )
+        Text(
+            text = label,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun StatCard(
+    content: @Composable ColumnScope.() -> Unit
+) {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainerLow
+        ),
+        modifier = Modifier
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            content = content,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(horizontal = 10.dp, vertical = 20.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun UserStatsScreenPreview() {
+    AppTheme {
+        UserStatsScreen()
+    }
 }
