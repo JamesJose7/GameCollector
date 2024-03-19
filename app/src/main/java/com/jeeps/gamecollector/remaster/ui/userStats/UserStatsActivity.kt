0de4e5d3a1aa5b6f6232e898295b4629d769c8e4
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2024-03-19 00:21:24 -0700

    Remove user stats views

--- app/src/main/java/com/jeeps/gamecollector/remaster/ui/userStats/UserStatsActivity.kt
+++ app/src/main/java/com/jeeps/gamecollector/remaster/ui/userStats/UserStatsActivity.kt
@@ -3,7 +3,6 @@
 package com.jeeps.gamecollector.remaster.ui.userStats
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -34,22 +33,15 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.compose.AppTheme
-import com.google.android.material.snackbar.Snackbar
-import com.jeeps.gamecollector.R
 import com.jeeps.gamecollector.databinding.ActivityStatsBinding
-import com.jeeps.gamecollector.databinding.ContentStatsBinding
 import com.jeeps.gamecollector.remaster.data.model.data.platforms.PlatformStats
 import com.jeeps.gamecollector.remaster.data.model.data.user.UserStats
-import com.jeeps.gamecollector.remaster.ui.adapters.PlatformStatsAdapter
 import com.jeeps.gamecollector.remaster.ui.base.BaseActivity
 import com.jeeps.gamecollector.remaster.ui.composables.CircularGraph
 import com.jeeps.gamecollector.remaster.utils.extensions.completionPercent
-import com.jeeps.gamecollector.remaster.utils.extensions.completionPercentage
 import com.jeeps.gamecollector.remaster.utils.extensions.setComposable
 import com.jeeps.gamecollector.remaster.utils.extensions.totalGames
-import com.jeeps.gamecollector.remaster.utils.extensions.value
 import com.jeeps.gamecollector.remaster.utils.extensions.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -58,13 +50,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class UserStatsActivity : BaseActivity() {
 
-    private val binding by viewBinding(ActivityStatsBinding::inflate).also {
-        // Workaround from view not initializing properly when view binding
-        if (it.isInitialized()) {
-            it.value.content.overallCompletionChart.show(listOf())
-        }
-    }
-    private lateinit var content: ContentStatsBinding
+    private val binding by viewBinding(ActivityStatsBinding::inflate)
 
     private val viewModel: UserStatsViewModel by viewModels()
 
@@ -73,76 +59,10 @@ class UserStatsActivity : BaseActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
         supportActionBar?.title = "Statistics"
-        content = binding.content
 
-        initializeCharts()
-        initializePlatformsRv()
-        initializeObservers()
 
         binding.screenCompose.setComposable { UserStatsScreen(viewModel) }
     }
-
-    private fun initializeCharts() {
-        val colors = intArrayOf(getColor(R.color.colorAccent))
-        content.overallCompletionChart.donutColors = colors
-        content.overallCompletionChart.animation.duration = 1000L
-        content.overallCompletionChart.animate(listOf(0f))
-    }
-
-    private fun initializePlatformsRv() {
-        val platformsRv = content.platformStatsRecyclerview
-        val layoutManager = object : LinearLayoutManager(this) {
-            override fun canScrollVertically(): Boolean {
-                return false
-            }
-        }
-        platformsRv.layoutManager = layoutManager
-        platformsRv.setHasFixedSize(true)
-    }
-
-    private fun initializeObservers() {
-        viewModel.isLoading.observe(this) {
-            content.statsProgressBar.visibility = if (it.value()) View.VISIBLE else View.GONE
-        }
-
-//        viewModel.userStats.observe(this) {
-//            it?.let {
-//                bindUserStats(it)
-//            }
-//        }
-    }
-
-    private fun bindUserStats(userStats: UserStats) {
-        if (userStats.platformStats.isEmpty()) {
-            Snackbar.make(
-                    binding.root,
-                    "You don't have any stats yet, Start by creating a platform",
-                    Snackbar.LENGTH_SHORT
-                )
-                .show()
-            return
-        }
-
-        // Overall stats
-        val completionPercentageText = "${userStats.completionPercentage()}%"
-        content.overallCompletionPercentage.text = completionPercentageText
-        content.overallTotal.text = userStats.totalGames().toString()
-        content.overallCompleted.text = userStats.completedGamesTotal.toString()
-        content.overallTotalPhysical.text = userStats.physicalTotal.toString()
-        content.overallTotalDigital.text = userStats.digitalTotal.toString()
-        content.overallLastGameCompleted.text = userStats.lastGameCompleted
-        // Chart
-        content.overallCompletionChart.animate(listOf(userStats.completionPercentage().toFloat()))
-
-        // Platform stats
-        setPlatformsAdapter(userStats.platformStats)
-    }
-
-    private fun setPlatformsAdapter(platformStats: List<PlatformStats>) {
-        val platformsStatsAdapter = PlatformStatsAdapter(platformStats)
-        content.platformStatsRecyclerview.adapter = platformsStatsAdapter
-    }
-
 }
 
 @Composable
