64b6fb3b6c418897413cdeaed14bcb9f620c70f7
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2024-05-17 17:29:20 -0700

    Lazy load user stats

--- app/src/main/java/com/jeeps/gamecollector/remaster/ui/userStats/UserStatsViewModel.kt
+++ app/src/main/java/com/jeeps/gamecollector/remaster/ui/userStats/UserStatsViewModel.kt
@@ -1,10 +1,8 @@
 package com.jeeps.gamecollector.remaster.ui.userStats
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.jeeps.gamecollector.remaster.data.model.data.user.UserStats
 import com.jeeps.gamecollector.remaster.data.State
+import com.jeeps.gamecollector.remaster.data.model.data.user.UserStats
 import com.jeeps.gamecollector.remaster.data.repository.AuthenticationRepository
 import com.jeeps.gamecollector.remaster.data.repository.UserStatsRepository
 import com.jeeps.gamecollector.remaster.ui.base.BaseViewModel
@@ -13,7 +11,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -24,11 +21,12 @@ class UserStatsViewModel @Inject constructor(
     private val authenticationRepository: AuthenticationRepository
 ) : BaseViewModel() {
 
-    private val _userStats = MutableStateFlow(UserStats())
-    val userStats: StateFlow<UserStats>
-        get() = _userStats.asStateFlow().also {
-            loadStats()
-        }
+    private val _userStats by lazy {
+        val userStats = MutableStateFlow(UserStats())
+        loadStats()
+        return@lazy userStats
+    }
+    val userStats: StateFlow<UserStats> = _userStats.asStateFlow()
 
     private fun loadStats() {
         val user = authenticationRepository.getUser() ?: return
